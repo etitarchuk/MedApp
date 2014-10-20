94b89d1c57cfe3833fbd075020c776e71c651ec1
sborodkin <sborodkin@usarnogroup.com>
2014-10-20 19:28:37 +0300

    Add Context

---
+++ MedApp.DataLayer/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MedApp.DataLayer.Models;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace MedApp.DataLayer
+{
+  public  class DataContext : DbContext
+    {
+      public DataContext()
+          : base("name=DataContext")
+                {
+                    Database.SetInitializer<DataContext>(new DropCreateDatabaseIfModelChanges<DataContext>());
+                }
+      public virtual DbSet<User> Users { get; set; }
+      public virtual DbSet<Role> Roles { get; set; }
+            
+    }
+}
--- MedApp.Web/Controllers/HomeController.cs
+++ MedApp.Web/Controllers/HomeController.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using MedApp.DataLayer;
 namespace MedApp.Web.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
+            using (DataContext context = new DataContext())
+            {
+                var d = context.Users.ToList();
+                var ds = context.Roles.ToList();
+            }
             return View();
         }
 
