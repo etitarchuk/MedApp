303bb3b53710d94358f254025be197a53c7e6499
etitarchuk <etitarchuk@usarnogroup.com>
2014-10-20 12:45:40 +0300

    Add Projects

---
+++ MedApp.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MedApp.Web.Startup))]
+namespace MedApp.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
