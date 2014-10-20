996565fb21df5f95e0a8c07f6feb7e4941d75505
sborodkin <sborodkin@usarnogroup.com>
2014-10-20 19:35:27 +0300

    Add models to DAL

---
+++ MedApp.DataLayer/Models/Role.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedApp.DataLayer.Models
+{
+    public class Role
+    {
+        [Key]
+        [DatabaseGeneratedAttribute(System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.Identity)]
+        public int Id { get; private set; }
+
+        [StringLength(50, MinimumLength = 3)]
+        public string RoleName { get; set; }
+
+        
+        public bool? IsAdmin { get; set; }
+       
+        public bool? IsOwner { get; set; }
+
+        public virtual ICollection<User> Users { get; set; }
+
+        
+
+    }
+}
---
+++ MedApp.DataLayer/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedApp.DataLayer.Models
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGeneratedAttribute(System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.Identity)]
+        public int Id { get; private set; }
+
+        [StringLength(50, MinimumLength = 3)]
+        public string Username { get; set; }
+
+        [StringLength(50, MinimumLength = 3)]
+        public string Password { get; set; }
+
+        public virtual Role  Role { get; set; }
+    }
+}
