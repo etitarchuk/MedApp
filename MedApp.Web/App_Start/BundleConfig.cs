6f916af09d989a3e7119090a58a29eb1de75d7bd
etitarchuk <etitarchuk@usarnogroup.com>
2014-10-22 19:02:10 +0300

    Change to layout

--- MedApp.Web/App_Start/BundleConfig.cs
+++ MedApp.Web/App_Start/BundleConfig.cs
@@ -27,6 +27,9 @@ namespace MedApp.Web
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
 
+            bundles.Add(new StyleBundle("~/bundles/custom").Include(
+                      "~/Content/layout.css"));
+
             // Set EnableOptimizations to false for debugging. For more information,
             // visit http://go.microsoft.com/fwlink/?LinkId=301862
             BundleTable.EnableOptimizations = true;
